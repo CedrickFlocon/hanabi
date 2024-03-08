34ba77343d078529658f096d72346791430b52e8
Cedrick Flocon <7161169+CedrickFlocon@users.noreply.github.com>
2024-03-08 10:15:30 +0100

    Deck distribution

--- game/src/main/kotlin/com/cedrickflocon/hanabi/game/Game.kt
+++ game/src/main/kotlin/com/cedrickflocon/hanabi/game/Game.kt
@@ -1,9 +1,17 @@
 package com.cedrickflocon.hanabi.game
 
-class Game {
+class Game(
+    val playerNumber: Int,
+    private val deck: Deck
+) {
 
     companion object {
         private const val HINT_MAX = 7
+        private val CARD_DISTRIBUTION = mapOf(2 to 5, 3 to 5, 4 to 4, 5 to 4)
+    }
+
+    init {
+        require(playerNumber in 2..5)
     }
 
     var turn: Int = 0
@@ -18,10 +26,18 @@ class Game {
     var hint: Int = HINT_MAX
         private set
 
+    private val _playerCard: Map<Int, MutableList<Card>> = (0..<playerNumber).associateWith {
+        (0..<CARD_DISTRIBUTION[playerNumber]!!)
+            .map { deck.pick() }
+            .toMutableList()
+    }
+    val playerCard: Map<Int, List<Card>> = _playerCard
+
     override fun toString(): String {
         return """
-        Turn : $turn | Score : $score
-        Life : $life | Hint: $hint
-        """.trimIndent()
+            |Turn : $turn | Score : $score
+            |Life : $life | Hint: $hint
+            |${playerCard.map { "Player ${it.key} : ${it.value}" }.joinToString("\n")}
+        """.trimMargin()
     }
 }
--- game/src/test/kotlin/com/cedrickflocon/hanabi/game/GameTest.kt
+++ game/src/test/kotlin/com/cedrickflocon/hanabi/game/GameTest.kt
@@ -1,27 +1,38 @@
 package com.cedrickflocon.hanabi.game
 
 import com.google.common.truth.Truth.assertThat
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.assertThrows
 
-class GameTest {
+class GameTest : DescribeSpec({
 
-    @Nested
-    inner class `Describe valid game` {
-        lateinit var game: Game
-
-        @BeforeEach
-        fun setUp() {
-            game = Game()
-        }
-
-        @Test
-        fun `it should initialize field`() {
-            assertThat(game.turn).isEqualTo(0)
-            assertThat(game.score).isEqualTo(0)
-            assertThat(game.life).isEqualTo(3)
-            assertThat(game.hint).isEqualTo(7)
+    describe("invalid player number") {
+        listOf(-1, 0, 1, 6).forEach {
+            it("it should throw an exception") {
+                assertThrows<IllegalArgumentException> { Game(it, mockk()) }
+            }
         }
     }
-}
+
+    describe("valid player number") {
+        listOf(2 to 5, 3 to 5, 4 to 4, 5 to 4).forEach { (playerNumber, cardNumber) ->
+            val cards = (0..<playerNumber * cardNumber).map { mockk<Card>() }
+            val deck = mockk<Deck> { every { pick() } returnsMany cards }
+            val game = Game(playerNumber, deck)
+
+            it("should initialize field for $playerNumber") {
+                assertThat(game.turn).isEqualTo(0)
+                assertThat(game.score).isEqualTo(0)
+                assertThat(game.life).isEqualTo(3)
+                assertThat(game.hint).isEqualTo(7)
+                cards.chunked(cardNumber).forEachIndexed { index, cards ->
+                    assertThat(game.playerCard[index]).isEqualTo(cards)
+                }
+                verify(exactly = playerNumber * cardNumber) { deck.pick() }
+            }
+        }
+    }
+})
